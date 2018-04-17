2de078e0eee940ad29df1dd0b927d8802264767c
saber110 <15675123342@163.com>
2018-04-17 16:15:42 +0800

    添加log

--- run.sh
+++ run.sh
@@ -6,19 +6,19 @@ result=$(echo $system | grep "raspberrypi")
 if [ "$result" != "" ]; then
   # run CMD parse
   cd $path/client/cmdOpt
-  ./cmdparse > cmdparse.log &
+  ./cmdparse > $path/log/cmdparse.log &
   # run ssh reserve proxy
   cd $path/client/ReserveProxyPort
   port=`./PortApply`
-  ssh -N -f -R $port:localhost:3333 ubuntu@123.206.64.174 -i /home/pi/laptop_win10 > ReserveProxyPort.log &
+  ssh -N -f -R $port:localhost:3333 ubuntu@123.206.64.174 -i /home/pi/laptop_win10 > $path/log/ReserveProxyPort.log &
 else
   # run python
   cd $path/server
-  python Graduation.py > PythonServer.log &
+  python Graduation.py > $path/log/PythonServer.log &
   # run CmdDistribution
   cd $path/server/CmdDistribution/
-  ./CmdDistribute > CmdDistribute.log &
+  ./CmdDistribute > $path/log/CmdDistribute.log &
   # run PortDistribution
   cd $path/server/PortDistribution
-  ./PortDistribute > PortDistribute.log&
+  ./PortDistribute > $path/log/PortDistribute.log&
 fi
