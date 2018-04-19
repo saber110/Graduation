133bc75869f0793eb5bebf3949d914e8ed154011
saber110 <15675123342@163.com>
2018-04-19 15:31:50 +0800

    树莓派运行通过

--- run.sh
+++ run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 path=$(cd `dirname $0`; pwd)
-pathOfInternet="/etc/network/if-up.d/"
 system=`uname -a`
 result=$(echo $system | grep "raspberrypi")
 if [ "$result" != "" ]; then
+  cd $path
   ./ReserveProxy.sh &
   # run CMD parse
   cd $path/client/cmdOpt
-  ./cmdparse > $path/log/cmdparse.log &
+  ./cmdparse >> $path/log/cmdparse.log &
 else
   # run python
   cd $path/server
-  python Graduation.py > $path/log/PythonServer.log &
+  python Graduation.py >> $path/log/PythonServer.log &
   # run CmdDistribution
   cd $path/server/CmdDistribution/
-  ./CmdDistribute > $path/log/CmdDistribute.log &
+  ./CmdDistribute >> $path/log/CmdDistribute.log &
   # run PortDistribution
   cd $path/server/PortDistribution
-  ./PortDistribute > $path/log/PortDistribute.log&
+  ./PortDistribute >> $path/log/PortDistribute.log&
 fi
