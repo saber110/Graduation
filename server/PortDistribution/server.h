6f476208275eefd1ccef963aa38b79710ac8de7a
saber110 <15675123342@163.com>
2018-05-26 16:22:24 +0800

    4444->4441

--- server/PortDistribution/server.h
+++ server/PortDistribution/server.h
@@ -1,7 +1,7 @@
 #ifndef _SERVER_H
 #define _SERVER_H
 
-#define SERVPORT 4444   // 服务器监听端口号
+#define SERVPORT 4441   // 服务器监听端口号
 #define BACKLOG 10  // 最大同时请求数
 #define MAXDATASIZE 1000
 #define UUIDLENGTH 15
